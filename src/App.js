9ad0fe0194a052650881447e615591010b80a52a
Anton Kuts <a.kuts.ibg@gmail.com>
2019-02-13 01:45:44 +0200

    only front 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import AddNewTask from './AddNewTask';
 import TasksList from './TasksList';
 import EditForm from './EditForm';
